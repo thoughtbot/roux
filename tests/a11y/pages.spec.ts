be508ba198b41b431cbf7036358cd744c946591e
Dave Iverson <daveiverson@thoughtbot.com>
2026-05-01 14:23:07 -0500

    Fixed tests and bugs after merging main
    - body styles were set - we need to override these for component previews to reduce the margins
    - dark mode was enabled, but we don't really have it supported yet. Force Playwright tests to run in light mode for now

--- tests/a11y/pages.spec.ts
+++ tests/a11y/pages.spec.ts
@@ -57,3 +57,16 @@ for (const example of examplePages) {
     await expect(page).toHaveScreenshot(`${name}.png`, { fullPage: true });
   });
 }
+
+test.describe.skip("dark mode snapshots", () => {
+  test.use({ colorScheme: "dark" });
+
+  for (const example of examplePages) {
+    const name = example.replace("/", "-").replace(".html", "");
+    test(`${name} - component example snapshot`, async ({ page }) => {
+      const response = await page.goto(`/examples/${example}`);
+      expect(response?.status()).toBe(200);
+      await expect(page).toHaveScreenshot(`dark-${name}.png`, { fullPage: true });
+    });
+  }
+});
